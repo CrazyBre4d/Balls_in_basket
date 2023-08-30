07097815e7e105439779e6eed1b79119233614f9
Vlas <vpkabakov@gmail.com>
2023-08-30 09:59:42 +0300

    Added Stream

--- src/main/java/vlas/Main.java
+++ src/main/java/vlas/Main.java
@@ -9,21 +9,13 @@ public class Main {
     public static void main (String[] args )
     {
         try {
-        Basket basket = new Basket(9);
-        BasketballBall basBall = new BasketballBall("b",3,true);
-        TennisBall tenBall = new TennisBall("a", 6,"blue");
-
-        List<Ball> balls = Arrays.asList(basBall, tenBall);
+        List<Ball> balls = Arrays.asList( new BasketballBall("leather",3,true),
+                                          new TennisBall("plastic", 6,"blue"),
+                                          new TennisBall("asdasf", 6,"blue"));
         balls.stream()
                         .sorted(Comparator.comparing(Ball::getMaterial))
                         .collect(Collectors.toList())
                         .forEach(System.out::println);
-
-        basket.set(0,basBall);
-        basket.set(1,tenBall);
-        System.out.println(basket);
-        //basBall.blowUp();
-        basBall.jump();
 
         } catch (RuntimeException | NoSuchFieldError e) {
             System.out.println(e);
